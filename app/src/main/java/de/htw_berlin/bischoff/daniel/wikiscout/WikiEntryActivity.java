35032e0e8b2b569e8ace0f36539ba467186ff88d
Daniel Bischoff <danielbischoff@hotmail.com>
2016-11-11 14:11:58 +0100

    added toolbar and back button to entry activity

--- app/src/main/java/de/htw_berlin/bischoff/daniel/wikiscout/WikiEntryActivity.java
+++ app/src/main/java/de/htw_berlin/bischoff/daniel/wikiscout/WikiEntryActivity.java
@@ -1,15 +1,27 @@
 package de.htw_berlin.bischoff.daniel.wikiscout;
 
 import android.net.Uri;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class WikiEntryActivity extends FragmentActivity implements WikiEntryFragment.OnFragmentInteractionListener {
+public class WikiEntryActivity extends AppCompatActivity implements WikiEntryFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiki_entry);
+
+        Toolbar Toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(Toolbar);
+
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // todo doesn't work yet :(
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
 
         if (findViewById(R.id.fragment_container) != null) {
 
@@ -22,6 +34,16 @@ public class WikiEntryActivity extends FragmentActivity implements WikiEntryFrag
             fragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
